dd0c3d58bdc4e2e4d2e6342a7b72764a7c2fd56a
Jannik Wienecke <wienecke.jannik@gmail.com>
2020-12-20 08:29:43 +0000

    make test pass

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,8 @@
-import { useSpotifyToken } from "app/context/AppProvider"
 import Layout from "app/layouts/Layout"
+import { SPOTIFY_AUTH_URL } from "app/spotifyConfig"
 import { Button } from "baseui/button"
-import { BlitzPage, Link } from "blitz"
+import { BlitzPage } from "blitz"
 import React from "react"
-import { SPOTIFY_AUTH_URL } from "../spotify.config"
-import { StyledLink } from "baseui/link"
 const Home: BlitzPage = () => {
   const redirectToSpotifyLogin = () => {
     window.location.assign(SPOTIFY_AUTH_URL)
