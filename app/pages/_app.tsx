9204822ae5ba6037fa5639ee47defbe0f33f7aa9
Jannik Wienecke <wienecke.jannik@gmail.com>
2020-12-21 09:44:07 +0000

    initialize empty custom theme

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,24 +1,39 @@
 import LoginForm from "app/auth/components/LoginForm"
 import { AppProvider } from "app/context/AppProvider"
-import { BaseProvider, LightTheme } from "baseui"
+import { BaseProvider, LightTheme, DarkTheme } from "baseui"
 import { AppProps, AuthenticationError, AuthorizationError, ErrorComponent, useRouter } from "blitz"
 import { ErrorBoundary, FallbackProps } from "react-error-boundary"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { Provider as StyletronProvider } from "styletron-react"
 import { styletron } from "../styletron"
+import React from "react"
+import { lightTheme } from "app/customTheme"
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = React.useState(lightTheme)
   const getLayout = Component.getLayout || ((page) => page)
   const router = useRouter()
 
+  const toggleTheme = () => {
+    if (theme === lightTheme) setTheme(DarkTheme)
+    else setTheme(lightTheme)
+  }
+
+  const nextTheme = theme === lightTheme ? "DarkTheme" : "LightTheme"
+
+  console.log(lightTheme.colors.accent100)
+  console.log(theme.colors.accent100)
+
   return (
     <StyletronProvider value={styletron}>
-      <BaseProvider theme={LightTheme}>
+      <BaseProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <AppProvider>
             <ErrorBoundary FallbackComponent={RootErrorFallback} resetKeys={[router.asPath]}>
-              {getLayout(<Component {...pageProps} />)}
+              {getLayout(
+                <Component toggleTheme={toggleTheme} nextTheme={nextTheme} {...pageProps} />
+              )}
             </ErrorBoundary>
           </AppProvider>
         </QueryClientProvider>
--- app/pages/testing/spotifyTest.tsx
+++ app/pages/testing/spotifyTest.tsx
@@ -1,21 +1,36 @@
+import { Box } from "app/components/baseComponents/Box"
 import Layout from "app/layouts/Layout"
 import { useSearch } from "app/spotifyAPI/useSearch"
-import React from "react"
+import { useStyletron } from "baseui"
+import { Block } from "baseui/block"
 import { Input } from "baseui/input"
+import { Theme } from "baseui/theme"
+import { DisplayLarge } from "baseui/typography"
 import { BlitzPage } from "blitz"
+import React from "react"
 
 interface Props {
   initialQuery?: string
+  nextTheme: string
+  toggleTheme: () => void
 }
 
-const TestPage: BlitzPage<Props> = ({ initialQuery = "Justin" }) => {
+const TestPage: BlitzPage<Props> = ({ nextTheme, toggleTheme, initialQuery = "Justin" }) => {
   const { search, searchResults, query } = useSearch<"artist">({
     searchType: "artist",
     options: { initialQuery },
   })
 
+  const [_, theme] = useStyletron()
   return (
     <>
+      <button onClick={toggleTheme}>Switch Theme</button>
+      <Box>
+        <div>nextTheme Theme : {nextTheme}</div>
+      </Box>
+      <Block color={theme.colors.accent} margin="100px">
+        <DisplayLarge>Helo</DisplayLarge>
+      </Block>
       <label htmlFor="search">Search</label>
       <Input value={query} onChange={(event: any) => search(event.target.value)} />
 
