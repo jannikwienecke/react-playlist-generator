b7dea95b131d21e2399d03084ebd5891abf95520
Jannik Wienecke <wienecke.jannik@gmail.com>
2020-12-21 08:36:01 +0000

    removed storybook - not working

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../app/components/**/*.stories.@(tsx)",
-  ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-}
