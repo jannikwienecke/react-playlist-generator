37712674e9cc1fc15524422d7827775f0d90ac67
Jannik Wienecke <wienecke.jannik@gmail.com>
2020-12-26 23:58:18 +0000

    fix env

--- app/spotifyConfig.ts
+++ app/spotifyConfig.ts
@@ -1,6 +1,6 @@
 const authEndpoint = "https://accounts.spotify.com/authorize"
 const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT
-export const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URL
+export const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URL as string
 // export const redirectUri = "http://localhost:3000/redirect"
 const scopes = [
   "user-top-read",
