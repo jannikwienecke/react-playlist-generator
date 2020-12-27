8ff981c194a81824a33b3f9f1a89782162847c9e
Jannik Wienecke <wienecke.jannik@gmail.com>
2020-12-27 01:10:00 +0000

    refactor promises

--- app/hooks/useFetchTracksFromManyAlbums.ts
+++ app/hooks/useFetchTracksFromManyAlbums.ts
@@ -12,16 +12,23 @@ export const useFetchTracksFromManyAlbums = (options?: PropsFetchTracksFromManyA
     async (albumIds: string[]) => {
       console.log("albumIds === ", albumIds)
 
-      const tracksPromise = await albumIds.map(async (albumId) => {
+      const promises: Promise<SpotifyApi.PagingObject<SpotifyApi.TrackObjectSimplified>>[] = []
+
+      albumIds.map(async (albumId) => {
         console.log("albumId:", albumId)
 
         const url = `albums/${albumId}/tracks?limit=${options?.limit || 50}`
-        const album: SpotifyApi.PagingObject<SpotifyApi.TrackObjectSimplified> = await client(url)
-        console.log("return album = ", album)
-        console.log("push items", album.items.length)
 
-        setTracks([...tracks, ...album.items])
+        promises.push(client(url))
       })
+
+      const albumList = await Promise.all(promises)
+      const trackList: SpotifyApi.TrackObjectSimplified[] = []
+      albumList.forEach((album) => trackList.push(...album.items))
+
+      console.log("trackList========", trackList)
+
+      setTracks(trackList)
     },
     [client, options]
   )
